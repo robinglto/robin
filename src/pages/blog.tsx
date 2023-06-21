557f15220927bbd949624a15d91b689b1f2ad0a4
robinglto <robinsomedia@pm.me>
2023-06-21 19:02:17 -0400

    wip

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -8,7 +8,7 @@ import { ArrowTopRightIcon, Link2Icon } from "@radix-ui/react-icons";
 
 function PostCard(post: any) {
   return (
-    <div className=" space-y-4 font-medium rounded-xl border border-transparent  hover:border-zinc-950 hover:bg-zinc-950 transition delay-50 duration-700 ease-in-out ">
+    <div className="p-2 space-y-4 font-medium rounded-xl border border-transparent  hover:border-zinc-950 hover:bg-zinc-950 transition delay-50 duration-700 ease-in-out ">
       <h2 className="mb-1 text-md sm:text-lg">
         <Link
           href={post.url}
