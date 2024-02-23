5772977a92c868a712b20e6f1959d03f2953a6e4
robinglto <robinsomedia@pm.me>
2024-02-23 16:55:31 -0400

    wip

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <div className="flex justify-end mt-auto">
       <div>
         <p className="subpixel-antialiased text-gray-600/90 dark:text-gray-400 text-xs text-center pb-20 dark:hover:text-white hover:text-black  transition duration-700 ease-in-out hover:cursor-pointer">
-          2024 © Robin.
+          2024 © Saul Perez
         </p>
       </div>
     </div>
